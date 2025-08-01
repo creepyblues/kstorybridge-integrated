27a21f15d2539ba0dc982361cbd61dc785a2c59d
Sungho Lee <sungholee@macbook-pro-55.lan>
2025-08-01 12:41:26 -0700

    admin app auth error fix3

--- apps/admin/src/integrations/supabase/client.ts
+++ apps/admin/src/integrations/supabase/client.ts
@@ -1,7 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './types'
 
-const supabaseUrl = "https://dlrnrgcoguxlkkcitlpd.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRscm5yZ2NvZ3V4bGtra2NpdGxwZCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzIxMzAzNzEyLCJleHAiOjIwMzY4Nzk3MTJ9.0YHIhCUWUZT7bYQ9PDa7cE3KJbsw-YJy_b_-SzU4n4w"
+const SUPABASE_URL = "https://dlrnrgcoguxlkkcitlpd.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRscm5yZ2NvZ3V4bGtrY2l0bHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3OTIzMzQsImV4cCI6MjA2NzM2ODMzNH0.KWYF7TvoA0I3iyoIbyYIyTSlJcIyPH6yCfHueEEMIlA";
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+})
